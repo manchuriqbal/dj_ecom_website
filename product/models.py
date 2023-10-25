d72ab52c7e747ab81de42364e05d599c0f4d91eb
manchuriqbal <manchur.fiverr@gmail.com>
2023-10-25 23:10:50 +0600

    complete fretured category, product and slider

--- product/models.py
+++ product/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Category(models.Model):
-    title = models.CharField(max_length=150, unique=True)
-    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
     featured = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
 
@@ -14,8 +14,8 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    title = models.CharField(max_length=150, unique=True)
-    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
     featured = models.BooleanField(default=False)
     discription = models.TextField(blank=True, null=True,)
     price = models.DecimalField(max_digits=8, decimal_places=2)
@@ -31,9 +31,10 @@ class Product(models.Model):
     
 class Slider(models.Model):
     title = models.CharField(max_length=50)
+    title2 = models.CharField(max_length=50, default='Lookbook.')
     banner = models.ImageField(upload_to='banners')
     show = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now_add=True)
     
     def __str__(self) -> str:
-        return self.title
+        return self.title2
--- product/views.py
+++ product/views.py
@@ -1,8 +1,21 @@
 from django.views import generic
 
+from .models import Category, Product, Slider
+
 
 class Home(generic.TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            {
+                'featured_category': Category.objects.filter(featured=True),
+                'featured_product': Product.objects.filter(featured=True),
+                'slider': Slider.objects.filter(show=True),
+            }
+        )
+        return context
 
 class ProductDetails(generic.TemplateView):
     template_name = 'product/product-page.html'
